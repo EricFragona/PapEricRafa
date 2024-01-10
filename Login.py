089f8492b2290d7a7783d961fa124ed886554776
EricFragona <156069088+EricFragona@users.noreply.github.com>
2024-01-10 11:58:38 +0000

    FormLogin

--- Login.py
+++ Login.py
@@ -1,25 +1,37 @@
-from tkinter import *
+import customtkinter as ctk
 import sqlite3
 
+#Funções-----------------------------------
+def mostar_pin():
+    if pinEntry.cget('show') == '':
+        pinEntry.configure(show='*')
+        visivel.configure(text='Mostrar')
+    else:
+        pinEntry.configure(show='')
+        visivel.configure(text='Esconder')
 
-app = Tk()
-app.geometry("300x300")
+
+app = ctk.CTk()
+app.geometry("450x350")
 app.title("Faça Login ou crie uma nova conta")
-
-titulo = Label(text="No Name Game")
-
-user = Label(app, text="Nome de Utilizador: ").grid(row=0, column=0)
-username = StringVar()
-usernameEntry = Entry(app, textvariable=username).grid(row=0, column=1)
-
-pin = Label(app, text="Pin: ").grid(row=2, column=0)
-username = StringVar()
-pinEntry = Entry(app, textvariable=username, show="*").grid(row=2, column=1)
+ctk.set_appearance_mode("dark")
+titulo = ctk.CTkLabel(app, text="No Name Game")
 
 
-
-
-
+#Inserir Dados-----------------------------
+user = ctk.CTkLabel(app, text="Nome de Utilizador: ")
+user.place(x= 90, y =110)
+username = ctk.StringVar()
+usernameEntry = ctk.CTkEntry(app, textvariable=username, placeholder_text="Utilizador", width=166)
+usernameEntry.place(x=210, y=110)
+pin = ctk.CTkLabel(app, text="Pin: ")
+pin.place(x=175, y=140)
+pinvar = ctk.StringVar()
+pinEntry = ctk.CTkEntry(app, textvariable=pinvar, show="*", placeholder_text="Pin", width=45 )
+pinEntry.place(x=210, y=140)
+visivel = ctk.CTkButton(app, text="Mostrar", width=100, command=mostar_pin)
+visivel.place(x=260, y=140)
+#-----------------------------------
 
 
 
