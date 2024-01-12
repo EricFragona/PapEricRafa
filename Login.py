0f8067d34f1cae8f982aec4e3822b64aa8df2b8b
EricFragona <156069088+EricFragona@users.noreply.github.com>
2024-01-12 12:57:13 +0000

    Update Login.py

--- Login.py
+++ Login.py
@@ -10,35 +10,40 @@ def mostar_pin():
         pinEntry.configure(show='')
         visivel.configure(text='Esconder')
 
-def tema():
-    if ctk.set_appearance_mode == "Dark":
+def tema(temaString):
+    if temaString == 1:
         ctk.set_appearance_mode = "Light"
+        temaString = 2
     else:
         ctk.set_appearance_mode = "Dark"
+        temaString = 1
 
 app = ctk.CTk()
+temaBool = True
 app.geometry("450x350")
 app.title("Faça Login ou crie uma nova conta")
-ctk.set_appearance_mode("dark")
+ctk.set_appearance_mode("light")
+temaString = 1
 titulo = ctk.CTkLabel(app, text="No Name Game")
+app.resizable(False, False)
 
 
 #Inserir Dados-----------------------------
-user = ctk.CTkLabel(app, text="Nome de Utilizador: ")
+user = ctk.CTkLabel(app, text="Nome de Utilizador : ")
 user.place(x= 90, y =110)
 username = ctk.StringVar()
-usernameEntry = ctk.CTkEntry(app, textvariable=username, placeholder_text="Utilizador", width=166)
+usernameEntry = ctk.CTkEntry(app, textvariable=username, placeholder_text="Utilizador", width=151)
 usernameEntry.place(x=210, y=110)
-pin = ctk.CTkLabel(app, text="Pin: ")
-pin.place(x=175, y=140)
+pin = ctk.CTkLabel(app, text="Pin de 4 dígitos : ")
+pin.place(x=110, y=140)
 pinvar = ctk.StringVar()
 pinEntry = ctk.CTkEntry(app, textvariable=pinvar, show="*", placeholder_text="Pin", width=45 )
 pinEntry.place(x=210, y=140)
 visivel = ctk.CTkButton(app, text="Mostrar", width=100, command=mostar_pin)
-visivel.place(x=260, y=160)
+visivel.place(x=260, y=140)
 
-btnTema = ctk.CTkButton(app, text="tema", width=100, command=tema)
-btnTema.place(x=260, y=140)
+btnTema = ctk.CTkButton(app, text="Tema", width=100, command= lambda: tema(temaString))
+btnTema.place(x=325, y=300)
 #-----------------------------------
 
 
