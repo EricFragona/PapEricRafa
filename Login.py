f6362d502ac754a18a4b4f2aafd9bcf293000706
EricFragona <156069088+EricFragona@users.noreply.github.com>
2024-01-15 12:03:05 +0000

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,52 +1,52 @@
 import customtkinter as ctk
+from CTkMenuBar import *
 import sqlite3
 
-#Funções-----------------------------------
+#Funções------------------------------------
 def mostar_pin():
     if pinEntry.cget('show') == '':
         pinEntry.configure(show='*')
-        visivel.configure(text='Mostrar')
+        BtnMostrar.configure(text='Mostrar')
     else:
         pinEntry.configure(show='')
-        visivel.configure(text='Esconder')
+        BtnMostrar.configure(text='Esconder')
 
-def tema(temaString):
-    if temaString == 1:
-        ctk.set_appearance_mode = "Light"
-        temaString = 2
-    else:
-        ctk.set_appearance_mode = "Dark"
-        temaString = 1
-
-app = ctk.CTk()
+def fechar():
+    root.quit()
+#App---------------------------------------
+root = ctk.CTk()
 temaBool = True
-app.geometry("450x350")
-app.title("Faça Login ou crie uma nova conta")
-ctk.set_appearance_mode("light")
+root.geometry("450x350")
+root.title("Faça Login ou crie uma nova conta")
+ctk.set_appearance_mode("system")
 temaString = 1
-titulo = ctk.CTkLabel(app, text="No Name Game")
-app.resizable(False, False)
+titulo = ctk.CTkLabel(root, text="No Name Game")
+root.resizable(False, False)
+
+#Menu--------------------------------------
+menu = CTkMenuBar(root)
+button_1 = menu.add_cascade("Definições")
+op1 = CustomDropdownMenu(widget=button_1)
+op1.add_option(option="Login")
+op1.add_option(option="Nova Conta")
+op1.add_option(option="Sair", command=fechar)
 
 
 #Inserir Dados-----------------------------
-user = ctk.CTkLabel(app, text="Nome de Utilizador : ")
+user = ctk.CTkLabel(root, text="Nome de Utilizador : ")
 user.place(x= 90, y =110)
 username = ctk.StringVar()
-usernameEntry = ctk.CTkEntry(app, textvariable=username, placeholder_text="Utilizador", width=151)
+usernameEntry = ctk.CTkEntry(root, textvariable=username, placeholder_text="Utilizador", width=151)
 usernameEntry.place(x=210, y=110)
-pin = ctk.CTkLabel(app, text="Pin de 4 dígitos : ")
-pin.place(x=110, y=140)
+pin = ctk.CTkLabel(root, text="Pin de 4 dígitos : ")
+pin.place(x=110, y=150)
 pinvar = ctk.StringVar()
-pinEntry = ctk.CTkEntry(app, textvariable=pinvar, show="*", placeholder_text="Pin", width=45 )
-pinEntry.place(x=210, y=140)
-visivel = ctk.CTkButton(app, text="Mostrar", width=100, command=mostar_pin)
-visivel.place(x=260, y=140)
-
-btnTema = ctk.CTkButton(app, text="Tema", width=100, command= lambda: tema(temaString))
-btnTema.place(x=325, y=300)
-#-----------------------------------
+pinEntry = ctk.CTkEntry(root, textvariable=pinvar, show="*", placeholder_text="Pin", width=45 )
+pinEntry.place(x=210, y=150)
+BtnMostrar = ctk.CTkButton(root, text="Mostrar", width=100, command=mostar_pin)
+BtnMostrar.place(x=260, y=150)
+BtnLogin = ctk.CTkButton(root, text="Login", width=100)
+BtnLogin.place(x=180, y=190)
 
 
-
-
-app.mainloop()
+root.mainloop()
